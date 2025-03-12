ce0264c02aedb8553664403fcae44bc50bde3858
jessica14angulo <ang21016@byui.edu>
2025-03-12 12:04:43 -0400

    eventListeners added

--- week02/scripts/bomjs.js
+++ week02/scripts/bomjs.js
@@ -1,15 +1,27 @@
-const { append } = require("express/lib/response");
-
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+//Adding functionality
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
 
-li.append(deleteButton);
+        li.append(deleteButton);
 
-list.append(li);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+});
+
