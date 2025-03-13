23bdd9cef8aebba7ddfb452ad45eb9108f4cca10
jessica14angulo <ang21016@byui.edu>
2025-03-13 19:31:01 -0400

    temples page week2

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+//Footer info
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
+
+//Hamburger Menu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
