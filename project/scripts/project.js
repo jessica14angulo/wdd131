a0183c8b4100e2e853381c7cc1020d8ed7122e8c
jessica14angulo <ang21016@byui.edu>
2025-04-10 13:31:53 -0400

    css for index finished

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,2 +1,11 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
+
+//Hamburger Menu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
